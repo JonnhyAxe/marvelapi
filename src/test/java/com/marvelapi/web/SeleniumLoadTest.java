bb05233edf7c1d3a751679f3750ad4ec27eb41f3
bld_jmsmachado <joaomiguelmachado@boldint.com>
2017-10-08 18:48:27 +0100

    MAPI: change the phantomjs.exe to internal location
    Change-Id: I0f22c09c93baa0e7643ef1a471d92b87bc1744a0

--- src/test/java/com/marvelapi/web/SeleniumLoadTest.java
+++ src/test/java/com/marvelapi/web/SeleniumLoadTest.java
@@ -1,6 +1,10 @@
 
 package com.marvelapi.web;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -14,20 +18,17 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-
 public class SeleniumLoadTest {
 
   private WebDriver driver;
 
 
     @Test()
-    public void getBenParkerPower_withSelenium() {
-
-        // driver = new FirefoxDriver();
+    public void getBenParkerPower_withSeleniumWithoutWebBrowser() {
 
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "C:\\dev\\tools\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "C:\\dev\\marvelapi\\src\\test\\resources\\phantomjs.exe");
         driver = new PhantomJSDriver(caps);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         final String urlOverHttps = "http://marvel.com/universe/Parker%2C_Ben_%28Uncle_Ben%29?utm_campaign=apiRef&utm_source=746d7e48cbc8d37cf4dd63c53296c3c1";
@@ -36,7 +37,10 @@ public class SeleniumLoadTest {
 
         WebElement search_button = driver.findElement(By.id("char-powers-content"));
 
-        String text = search_button.getText();
+        String powerText = search_button.getText();
+
+        assertThat(powerText, notNullValue());
+        assertThat(powerText, equalTo(""));
 
         driver.quit();
 
